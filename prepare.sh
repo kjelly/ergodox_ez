25a4e8a4ca923e1ee054a6c2cc0b0c6af3195993
Kuo-tung Kao <ya790206@gmail.com>
2017-07-23 16:35:28 +0800

    update dependency

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install gcc-avr avr-libc dfu-programmer
+sudo apt-get install gcc-avr avr-libc dfu-programmer libusb-dev
 git submodule update --init
 cd teensy_loader_cli
 make
